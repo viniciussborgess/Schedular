92ea5d1c89028f8c4b8656d470e89361546c0b25
viniciussborgess <viniborges2005@gmail.com>
2025-01-09 18:29:50 -0300

    Remove

--- Schedular.Domain/obj/Debug/net8.0/Schedular.Domain.AssemblyInfo.cs
+++ Schedular.Domain/obj/Debug/net8.0/Schedular.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schedular.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4feb8ad035fdc3568048224e9fdb2d1627a9708d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d43c9bc0d2718d5ba6cb0dd004ca1c98c01bf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schedular.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Schedular.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
